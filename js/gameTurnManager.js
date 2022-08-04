8add6e26c724798b415b1e92780a43aa2e102439
SoulThy <daniel.majewski04@gmail.com>
2022-08-04 20:51:27 +0200

    Implemented first move of "AI"

--- js/gameTurnManager.js
+++ js/gameTurnManager.js
@@ -1,6 +1,22 @@
 function gameTurnManager() {
-  if (gameActive) {
+  if (gameActive === false) return;
+
+  if (aiCheckbox.checked === false) {
     currentPlayer = currentPlayer == "X" ? "O" : "X";
     gameStatus.innerHTML = currentPlayerTurn();
+  } else {
+    let cellsAvailabe = [];
+    cellsStatus.forEach((x, index) => {
+      if (x === "") {
+        cellsAvailabe.push(index);
+      }
+    });
+    if (cellsStatus.filter((x) => x == "X").length == 1) {
+      rngValue = Math.floor(Math.random() * cellsAvailabe.length); //WRONG
+      cellsStatus[cellsAvailabe[rngValue]] = "O";
+      document.querySelector(
+        `[grid-cell-index="${cellsAvailabe[rngValue]}"]`
+      ).innerHTML = "O";
+    }
   }
 }
